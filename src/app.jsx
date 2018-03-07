76352d10c45ff7a8ac8510236623e4b59fc83c19
Heng Ly Aun <aunhengly@gmail.com>
2018-03-06 18:39:31 -0800

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div id="tobefixed">
+          <header>
+            <h1>San Diego Top Sports</h1>
+            <p>A list of the top 30 places to see in San Diego, California.</p>
+          </header>
+        </div>
+
+        <div id="top-spots-container">
+          {/* <pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre> */}
+          {
+            this.state.topspots.map(topspot => (
+              <TopSpot
+                key={topspot.id}
+                name={topspot.name}
+                description={topspot.description}
+                location={topspot.location} />
+            ))
+          }
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/topspot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div className="well">
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+        <a href={"https://maps.google.com/?q="+props.location[0]+","+props.location[1]} target="blank" id="push">VIEW MAP</a>
+    </div>
+    )
+};
